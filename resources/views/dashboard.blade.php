789bb347112055e8f4ffd752460b833927ff6413
shoreful-mardy <smbisal6037@gmail.com>
2024-08-20 21:21:58 +0600

    Admin Dashboard Segment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,41 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+           <h2>HI.. {{Auth::user()->name}}</h2>
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+       <div class="container">
+        <div class="row">
+
+           <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Create Date</th>
+    </tr>
+  </thead>
+  <tbody>
+@php
+    $user = App\Models\User::all();
+@endphp
+   <h2>Total User :<span class="text-danger">{{count($user)}}</span></h2>
+      @foreach($user as $key => $item)
+       <tr>
+      <th scope="row">{{$key+1}}</th>
+      <td>{{$item->name}}</td>
+      <td>{{$item->email}}</td>
+      <td>{{ $item->created_at->format('d/m/Y') }}</td>
+       </tr>
+      @endforeach
+   
+
+  </tbody>
+</table> 
         </div>
+       </div>
     </div>
 </x-app-layout>
