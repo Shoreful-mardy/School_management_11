d39853bf7d32b3ec4edbeb5056735d1fcc09abcc
shoreful-mardy <smbisal6037@gmail.com>
2024-08-23 20:10:10 +0600

    Sidebar menu Active Deactive Option

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,7 @@
+ @php
+
+ @endphp
+
   <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">	
@@ -17,14 +21,14 @@
       <!-- sidebar menu-->
       <ul class="sidebar-menu" data-widget="tree">  
 		  
-		<li>
-          <a href="index.html">
+		<li class="{{ Route::currentRouteName() === 'dashboard' ? 'active' : '' }}">
+          <a href="{{ route('dashboard') }}">
             <i data-feather="pie-chart"></i>
 			<span>Dashboard</span>
           </a>
         </li>  
 		
-        <li class="treeview">
+        <li class="treeview {{ Route::currentRouteName() === 'user.view' || Route::currentRouteName() === 'add.user' ? 'active' : '' }}">
           <a href="#">
             <i data-feather="message-circle"></i>
             <span>Manage User</span>
@@ -38,7 +42,7 @@
           </ul>
         </li> 
 		  
-        <li class="treeview">
+        <li class="treeview {{ Route::currentRouteName() === 'profile.view' || Route::currentRouteName() === 'password.view' ? 'active' : '' }}">
           <a href="#">
             <i data-feather="mail"></i> <span>Manage Profile</span>
             <span class="pull-right-container">
