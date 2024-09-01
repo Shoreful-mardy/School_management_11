1c4be0cf840df4e408e5babb0bc8b373fa46795c
shoreful-mardy <smbisal6037@gmail.com>
2024-09-01 10:53:54 +0600

    Student View Page Modified

--- app/Models/AssignStudent.php
+++ app/Models/AssignStudent.php
@@ -12,4 +12,10 @@ class AssignStudent extends Model
     public function student(){
         return $this->belongsTo(User::class,'studetn_id','id');
     }
+    public function class(){
+        return $this->belongsTo(StudentClass::class,'class_id','id');
+    }
+    public function year(){
+        return $this->belongsTo(StudentYear::class,'year_id','id');
+    }
 }
--- resources/views/backend/student/student_reg/student_view.blade.php
+++ resources/views/backend/student/student_reg/student_view.blade.php
@@ -23,7 +23,7 @@
 											<select name="year_id" id="year_id"  class="form-control">
 												<option value="" selected="" disabled="">Select Year</option>
 												@foreach($student_year as $item)
-												<option value="{{$item->id}}">{{$item->name}}</option>
+												<option value="{{$item->id}}" {{ $item->year_id == $year_id ? 'selected' : '' }}>{{$item->name}}</option>
 												@endforeach
 											</select>
 										</div>
@@ -36,7 +36,7 @@
 											<select name="class_id" id="class_id"  class="form-control">
 												<option value="" selected="" disabled="">Select Class</option>
 												@foreach($student_class as $item)
-												<option value="{{$item->id}}">{{$item->name}}</option>
+												<option value="{{$item->id}}" {{ (@$class_id == $item->class_id) ? 'selected' : '' }}>{{$item->name}}</option>
 												@endforeach
 											</select>
 										</div>
@@ -70,17 +70,31 @@
 						<thead>
 							<tr>
 								<th width="5%">Sl</th>
+								<th>Image</th>
 								<th>Name</th>
 								<th>Id No</th>
-								<th width="25%">Action</th>
+								<th>Roll</th>
+								<th>Year</th>
+								<th>Class</th>
+								@if(Auth::user()->role == 'Admin')
+								<th>Code</th>
+								@endif
+								<th width="20%">Action</th>
 							</tr>
 						</thead>
 						<tbody>
 							@foreach($data as $key => $item)
 							<tr>
 								<td>{{ $key+1}}</td>
+								<td>
+									<img src="{{ (!empty($item->student->photo))? url('upload/student_images/'.$item->student->photo) : url('upload/no_image.jpg')}}" style="height: 60px; width: 60px;">
+								</td>
 								<td>{{ $item->student->name}}</td>
 								<td>{{ $item->student->id_no}}</td>
+								<td>{{ $item->roll}} </td>
+								<td>{{ $item->year->name}}</td>
+								<td>{{ $item->class->name}}</td>
+								<td>{{ $item->student->code}}</td>
 								<td>
 									<a href="{{ route('edit.exam.type',$item->id) }}" class="btn btn-info">Edit</a>
 									<a href="{{ route('delete.exam.type',$item->id) }}" id="delete" class="btn btn-danger">Delete</a>
@@ -91,9 +105,16 @@
 						<tfoot>
 							<tr>
 								<th width="5%">Sl</th>
+								<th>Image</th>
 								<th>Name</th>
 								<th>Id No</th>
-								<th width="25%">Action</th>
+								<th>Roll</th>
+								<th>Year</th>
+								<th>Class</th>
+								@if(Auth::user()->role == 'Admin')
+								<th>Code</th>
+								@endif
+								<th width="20%">Action</th>
 							</tr>
 						</tfoot>
 					  </table>
