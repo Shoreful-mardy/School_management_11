08ee0f9d33e90233cfa6039d4533a48796e59fc0
shoreful-mardy <smbisal6037@gmail.com>
2024-09-05 22:47:44 +0600

    Leave Purpose field Modified

--- resources/views/backend/employee/employee_leave/employee_leave_add.blade.php
+++ resources/views/backend/employee/employee_leave/employee_leave_add.blade.php
@@ -1,6 +1,6 @@
  @extends('admin.admin_master')
  @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
  <div class="content-wrapper">
 	  <div class="container-full">
 		<!-- Content Header (Page header) -->
@@ -37,12 +37,14 @@
 							<div class="form-group">
 								<h5>Leave Purpose<span class="text-danger">*</span></h5>
 								<div class="controls">
-									<select name="leave_purpose_id" required="" class="form-control">
+									<select name="leave_purpose_id" id="leave_purpose_id" class="form-control">
 										<option value="" selected="" disabled="">Select Purpose</option>
 										@foreach($leave_purpose as $item)
 										<option value="{{ $item->id}}">{{ $item->name}}</option>
 										@endforeach
+										<option value="0">New Purpose</option>
 									</select>
+									<input type="text" name="name" id="add_another" class="form-control" placeholder="Write Purpose" style="display: none;">
 								</div>
 							</div>
 						</div><!--End col-md-6-->
@@ -83,5 +85,16 @@
 		<!-- /.content -->
 	  </div>
   </div>
-
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(document).on('change','#leave_purpose_id',function(){
+			var leave_purpose_id = $(this).val();
+			if(leave_purpose_id == 0){
+				$('#add_another').show();
+			}else{
+				$('#add_another').hide();
+			}
+		})
+	})
+</script>
 @endsection
